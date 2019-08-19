bd82a1f3e378f93c1cc7f2e1332081eae812a904
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-19 22:21:16 +0000

    Used template literals, fixed emojies, and removed tests from assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🛑 Assertion passed - ' + actual + ' = ' + expected);
+    console.log(`✅ Assertion passed -  ${actual} = ${expected}`);
   } else {
-    console.log('✅ Assertion failed - ' + actual + ' != ' + expected);
+    console.log(`🛑 Assertion failed - ${actual} != ${expected}`);
   }
 };
-
-//Tests
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual('Hello World', 'Hello World');
-assertEqual(1, 10);
-assertEqual('25', 25);
