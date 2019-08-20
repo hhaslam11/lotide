ae0830e85620f72a70ab7fb258af5483aeec4b3d
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-20 21:10:31 +0000

    Fixed formatting in console.log to show the data in the objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -27,5 +27,28 @@ const eqArrays = function(arrActual, arrExpected) {
 };
 
 const assertObjectsEqual = function(objectActual, objectExpected) {
-  console.log(eqObjects(objectActual, objectExpected) ? `✅ Assertion passed -  ${objectActual} = ${objectExpected}` : `🛑 Assertion failed - ${objectActual} != ${objectExpected}`);
+  const inspect = require('util').inspect;
+  console.log(eqObjects(objectActual, objectExpected) ? `✅ Assertion passed -  ${inspect(objectActual)} = ${inspect(objectExpected)}` : `🛑 Assertion failed - ${inspect(objectActual)} != ${inspect(objectExpected)}`);
 };
+
+//tests
+const a = {
+  a: "1",
+  b: "1.5",
+  c: [1, 2, 3]
+};
+
+const b = {
+  a: "1",
+  b: "2",
+  c: "3"
+};
+
+const c = {
+  a: "1",
+  b: "1.5",
+  c: [1, 2, 3]
+};
+
+assertObjectsEqual(a, b); //false
+assertObjectsEqual(a, c); //true
