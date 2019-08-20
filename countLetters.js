5886eac1f98573b2f5418e775e64b4ad62736b17
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-20 16:32:02 +0000

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+const countLetters = function(inputString) {
+  const results = {};
+  for (let letter of inputString) {
+    if (letter === ' ') continue; //skip spaces
+
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
