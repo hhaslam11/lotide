4466e90847be82da5a0c9f39436da99631e7a0eb
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-20 11:32:32 +0000

    Added assertEqualArray.js

---
+++ assertEqualArrays.js
@@ -0,0 +1,9 @@
+const eqArrays = function(arrActual, arrExpected) {
+  let isEqual = true;
+  if (arrActual.length !== arrExpected.length) isEqual = false;
+  
+  for (let i = 0; i < arrActual.length; i++)
+    if (arrActual[i] !== arrExpected[i]) isEqual = false;
+  
+  console.log(isEqual ? `✅ Assertion passed -  ${arrActual} = ${arrExpected}` : `🛑 Assertion failed - ${arrActual} != ${arrExpected}`);
+};
