3f0ededfb50479c257dc96d3b55650dfd88c28b1
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-20 12:33:18 +0000

    Added without.js

---
+++ without.js
@@ -0,0 +1,15 @@
+const without = function(source, valuesToRemove) {
+  let filteredArray = [];
+  let matchingValues;
+
+  for (let sourceIndex in source) {
+    matchingValues = false;
+    
+    for (let valuesToRemoveIndex in valuesToRemove) {
+      if (source[sourceIndex] === valuesToRemove[valuesToRemoveIndex])
+        matchingValues = true;
+    }
+    if (!matchingValues) filteredArray.push(source[sourceIndex]);
+  }
+  return filteredArray;
+};
