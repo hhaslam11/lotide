09c764e4b617a80bdd5dfeadeb216402513e54fb
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-21 11:18:41 +0000

    fixed bug in eqObjects (not comparing lengths)

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,8 @@
 const eqObjects = function(object1, object2) {
   for (let objectIndex in object1) { //loops through object1
 
+    if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+
     //if object1 is an array, compare using eqArray()
     if (Array.isArray(object1[objectIndex])) {
       if (!eqArrays(object1[objectIndex], object2[objectIndex])) {
@@ -16,6 +18,8 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+
+
 //eqArray.js
 const eqArrays = function(arrActual, arrExpected) {
   if (arrActual.length !== arrExpected.length) return false;
@@ -25,3 +29,11 @@ const eqArrays = function(arrActual, arrExpected) {
   
   return true;
 };
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
+
