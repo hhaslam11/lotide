dc435fb6c5204d0d5bd4fe3cd385940c7bb61c2d
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-23 20:49:03 +0000

    Made eqArray capable of comparing nested arrays using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion passed -  ${actual} = ${expected}`);
-  } else {
-    console.log(`🛑 Assertion failed - ${actual} != ${expected}`);
-  }
-};
 const eqArrays = function(arrActual, arrExpected) {
   if (arrActual.length !== arrExpected.length) return false;
   
-  for (let i = 0; i < arrActual.length; i++)
-    if (arrActual[i] !== arrExpected[i]) return false;
+  for (let i = 0; i < arrActual.length; i++) {
+    
+    //if arrActual[i] is another array, compare with recursion
+    if (Array.isArray(arrActual[i])) {
+      if (!eqArrays(arrActual[i], arrExpected[i])) return false;
+
+    //else, compare using ===
+    } else if (arrActual[i] !== arrExpected[i]) return false;
+  }
   
   return true;
 };
