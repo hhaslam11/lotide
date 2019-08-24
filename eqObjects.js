78146741490a34efa74fe8260f02d57640f8fcd1
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-24 11:16:34 +0000

    reverted back to previous commit. this just broke it

--- eqObjects.js
+++ eqObjects.js
@@ -12,23 +12,25 @@ const eqArrays = function(arrActual, arrExpected) {
 const eqObjects = function(object1, object2) {
   
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-  if (typeof object1 !== 'object' || typeof object2 !== 'object') return false;
-
+  
   for (let objectIndex in object1) { //loops through object1
 
     //if object1[objectIndex] is an array, compare using eqArray()
     if (Array.isArray(object1[objectIndex])) {
-      if (!eqArrays(object1[objectIndex], object2[objectIndex])) return false;
-    }
+      if (!eqArrays(object1[objectIndex], object2[objectIndex])) {
+        return false;
+      }
 
     //if object1[objectIndex] is another object, compare using recursion
-    if (typeof object1[objectIndex] === 'object') {
+    } else if (typeof object1[objectIndex] === 'object') {
       if (!eqObjects(object1[objectIndex], object2[objectIndex])) return false;
-    }
+
 
     //if object1[objectIndex] is not an array, compare using ===
-    if (object1[objectIndex] !== object2[objectIndex]) return false;
-  
+    } else if (object1[objectIndex] !== object2[objectIndex]) {
+      return false;
+    }
+
   }
   return true;
 };
