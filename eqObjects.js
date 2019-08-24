e5a223e33f1f92e183dce02c5ee9d1c451da4f36
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-24 10:52:36 +0000

    cleaned up eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -12,25 +12,24 @@ const eqArrays = function(arrActual, arrExpected) {
 const eqObjects = function(object1, object2) {
   
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-  
+  if (typeof object1 !== 'object' || typeof object2 !== 'object') return false;
+
   for (let objectIndex in object1) { //loops through object1
 
-    //if object1 is an array, compare using eqArray()
+    //if object1[objectIndex] is an array, compare using eqArray()
     if (Array.isArray(object1[objectIndex])) {
-      if (!eqArrays(object1[objectIndex], object2[objectIndex])) {
-        return false;
-      }
-
-    //if object1 is another object, compare using recursion
-    } else if (typeof object1[objectIndex] === 'object') {
-      return eqObjects(object1[objectIndex], object2[objectIndex]);
-
-
-    //if object1 is not an array, compare using ===
-    } else if (object1[objectIndex] !== object2[objectIndex]) {
-      return false;
+      if (!eqArrays(object1[objectIndex], object2[objectIndex])) return false;
     }
 
+    //if object1[objectIndex] is another object, compare using recursion
+    if (typeof object1[objectIndex] === 'object') {
+      if (!eqObjects(object1[objectIndex], object2[objectIndex])) return false;
+    }
+
+    //if object1[objectIndex] is not an array, compare using ===
+    if (object1[objectIndex] !== object2[objectIndex]) return false;
+  
   }
   return true;
 };
+
