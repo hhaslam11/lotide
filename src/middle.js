9b231d96bf9c5a594327d62e8bb480b9c3683dc7
Kaleb Haslam <kalebmarc@hotmail.com>
2019-08-26 20:52:50 +0000

    Added mocha tests for middle.js with @MaxWong03

--- src/middle.js
+++ src/middle.js
@@ -18,3 +18,5 @@ const middle = function(arr) {
 const isEven = function(number) {
   return number % 2 === 0;
 };
+
+module.exports = middle;
---
+++ test/middle.test.js
@@ -0,0 +1,28 @@
+const middle = require('../src/middle');
+const assert = require('chai').assert;
+
+//test case
+describe('middle', () => {
+  it('should return [2] when input array is [1,2,3]', () => {
+    assert.deepEqual(middle([1,2,3]), 2);
+  });
+  it('it should return [2,3] when input array is [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it('it should return empty array when input array length is <=2 ', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it('it should return empty array when input array length is <=2 ', () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it('it should return empty array when input array length is <=2 ', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
+
+
+/*
+it('', () => {
+
+});
+*/
